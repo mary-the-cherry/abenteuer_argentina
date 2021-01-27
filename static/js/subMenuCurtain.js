6f5c8d3355e9ce0937de4c28060f4238267e668b
Mary-the-cherry <meyer.maren100@googlemail.com>
2021-01-27 16:06:28 -0300

    Update navigation/footer for section Reiseplanung

--- static/js/subMenuCurtain.js
+++ static/js/subMenuCurtain.js
@@ -1,3 +1,5 @@
+// Overlay Reiseziele
+
 let openBtn = document.querySelector(".openSubNav");
 openBtn.addEventListener("click", () => {
 	showNav();
@@ -8,15 +10,26 @@ closeBtn.addEventListener("click", () => {
    hideNav();
 });
 
+let openBtnPl = document.querySelector(".openSubNavPlanung");
+openBtnPl.addEventListener("click", () => {
+	showNavPl();
+});
+
+let closeBtnPl = document.querySelector(".closeBtnPlanung");
+closeBtnPl.addEventListener("click", () => {
+	hideNav();
+});
 		
 const navOverlay = document.querySelector("#nav-overlay");
+const navOverlayPl = document.querySelector("#nav-overlay-planung");
 const body = document.body;
 let navbar = document.getElementById("navbar_show");
 
 
 function showNav() {
    if (navOverlay.style.height > "100"){
-	   navOverlay.style.height="0";
+	   navOverlay.style.height = "0";
+	   navOverlay.style.zIndex = "50";
 	   body.style.overflow="auto";
 	   body.focus();
    }
@@ -26,20 +39,48 @@ function showNav() {
 	   let yPosition = window.scrollY;
 	   let xPosition = window.scrollX;
 	   window.scroll(xPosition,yPosition-10);
-	   navOverlay.style.overflow="hidden";
-	   navOverlay.style.height="100%";
-	   navOverlay.style.overflow="auto";
+	   navOverlay.style.overflow = "hidden";
+	   navOverlayPl.style.height = "0";
+	   navOverlay.style.height = "100%";
+	   navOverlay.style.overflow = "auto";
+	   navOverlay.style.zIndex = "51";
 	   secureNavbarView();
    }
 }
 
+
+function showNavPl() {
+	if (navOverlayPl.style.height > "100") {
+		navOverlayPl.style.zIndex = "50";
+		navOverlayPl.style.height = "0";
+		navOverlay.style.height = "0";
+		body.style.overflow = "auto";
+		body.focus();
+	}
+	else {
+		body.style.overflow = "hidden";
+		let yPosition = window.scrollY;
+		let xPosition = window.scrollX;
+		window.scroll(xPosition, yPosition - 10);
+		navOverlayPl.style.overflow = "hidden";
+		navOverlay.style.height = "0";
+		navOverlayPl.style.height = "100%";
+		navOverlayPl.style.zIndex = "51";
+		navOverlayPl.style.overflow = "auto";
+		secureNavbarView();
+	}
+}
+
 function hideNav() {
-	navOverlay.style.overflow="hidden";
-	navOverlay.style.height="0";
-	body.style.overflow="auto";
+	navOverlay.style.overflow = "hidden";
+	navOverlay.style.height = "0";
+	navOverlayPl.style.height = "0";
+	navOverlayPl.style.zIndex = "50";
+	navOverlay.style.zIndex = "50";
+	body.style.overflow = "auto";
 	body.focus();
 }
 
-function secureNavbarView(){
+function secureNavbarView() {
 	navbar.style.top = "0";
 }
