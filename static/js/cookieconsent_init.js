7b61be76c67ec579982823639dbc6ad9480bbe73
Mary-the-cherry <meyer.maren100@googlemail.com>
2021-06-18 19:17:45 +0200

    Update Cookie Tool

--- static/js/cookieconsent_init.js
+++ static/js/cookieconsent_init.js
@@ -19,5 +19,46 @@ window.cookieconsent.initialise({
       allow:'Gib mir Cookies!',
      deny: 'Nur essentielle Cookies',
     href: 'https://abenteuer-argentina.de/datenschutzerklaerung.html',
-  },
+    },
+    onInitialise: function (status) {
+        var type = this.options.type;
+        var didConsent = this.hasConsented();
+        if (type == 'opt-in' && didConsent) {
+            console.log("1. Enable");
+            window['ga-disable-UA-175425475-1'] = false;
+        }
+        if (type == 'opt-out' && !didConsent) {
+            // disable cookies
+            console.log("1. Disable");
+            window['ga-disable-UA-175425475-1'] = true;
+        }
+    },
+    onStatusChange: function (status, chosenBefore) {
+        var type = this.options.type;
+        var didConsent = this.hasConsented();
+        if (type == 'opt-in' && didConsent) {
+            // enable cookies
+            console.log("2. Enable");
+            window['ga-disable-UA-175425475-1'] = false;
+        }
+        if (type == 'opt-out' && !didConsent) {
+            // disable cookies
+            console.log("2. Disable");
+            window['ga-disable-UA-175425475-1'] = true;
+        }
+    },
+    onRevokeChoice: function () {
+        var type = this.options.type;
+        if (type == 'opt-in') {
+            // disable cookies
+            console.log("3. Disable");
+            window['ga-disable-UA-175425475-1'] = true;
+        }
+        if (type == 'opt-out') {
+            // enable cookies
+            console.log("3. Enable");
+            window['ga-disable-UA-175425475-1'] = false;
+        }
+    },
+
 });
